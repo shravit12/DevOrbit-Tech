6832f6605193bb1d1f79ecff946540d9a36ec7e1
Shravit Sharma <138794026+shravit12@users.noreply.github.com>
2026-04-02 19:24:37 +0530

    Fix formatting and ensure export of nextConfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-images: {
-  remotePatterns: [
-    {
-      protocol: "https",
-      hostname: "images.pexels.com",
-    },
-  ],
-}
-  reactCompiler: true,
+  reactStrictMode: true,
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
